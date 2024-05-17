1b102ba51f99d13652df40013fcd4d10f95052fc
Sovietroll <142727276+Sovietroll@users.noreply.github.com>
2024-05-18 02:29:42 +0800

    Update prototypeShelf.js

--- prototypeShelf.js
+++ prototypeShelf.js
@@ -307,7 +307,7 @@ quantityL1.innerHTML = '-';
 // dateL1 = dateEXP(expDateL1, 2024,(1-1), 12, '');
 
 //! L2
-nameL2.innerHTML = 'Shaker Gold';
+nameL2.innerHTML = 'Shaker Goldd';
 quantityL2.innerHTML = 'x4';
 // dateL2 = dateEXP(expDateL2, 2024,(1-1), 12, '');
 
